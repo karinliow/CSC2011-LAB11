9ac3560e1ae09d730d039d88e2eb81eb0714aded
karin <karinliow@users.noreply.github.com>
2021-11-18 10:43:29 +0800

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        string name = "karin";
+        ViewBag.Message = "hello " + name;
         return View();
     }
 
