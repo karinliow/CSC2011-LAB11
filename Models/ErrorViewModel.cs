d0088d88196a7600188516ee28c66d02d7ad478d
karin <karinliow@users.noreply.github.com>
2021-11-18 10:24:17 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSC2011_LAB11.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
